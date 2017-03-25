3781bc4a58bba2815b5034092aa540252fd7207e
Tom Zhu <zhu_shixin@126.com>
2017-03-25 22:23:43 +0800

    2017.3.25tidy up

--- js/functions.js
+++ js/functions.js
@@ -3,12 +3,12 @@ $(function(){
 	// select默认选中项颜色
 	var unSelected = "#999";
 	var selected = "#333";
-	$("select").css("color", unSelected);
+	$("select").css("color", selected);
 	$("option").css("color", selected);
 	$("select").change(function () {
 		var selItem = $(this).val();
 		if (selItem == $(this).find('option:first').val()) {
-			$(this).css("color", unSelected);
+			$(this).css("color", selected);
 		} else {
 			$(this).css("color", selected);
 		}
